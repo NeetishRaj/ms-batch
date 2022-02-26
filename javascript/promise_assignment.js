d172703952fc1516d46f513bbe6353d1d02ab065
Neetish Raj <neetishop@gmail.com>
2022-02-27 00:16:18 +0530

    Added notes for fetch, promise

--- javascript/promise_assignment.js
+++
@@ -1,6 +0,0 @@
-// Create a 3 step process time process to simulate making a sandwitch
-//  1. get_ingredients() - 3 seconds 
-//  2. toast_two_breads() - 2 seconds
-//  3. apply_peanut_butter_mix() - 4 seconds
-
-// Write this logic using Promise syntax

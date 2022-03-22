a359939adb1ae77653b4315e31910cc242c85c55
Neetish Raj <neetishop@gmail.com>
2022-03-22 23:11:29 +0530

    Added final notes for SQL course

---
+++ sql_server/SQL_assignment2.sql
@@ -0,0 +1,43 @@
+-- USE w3_db dataset to solve all the queries
+
+-- QUERY 1
+-- Create a stored procedure which consists of a cmplete sql code to create 'Suppliers' table
+-- and insert the 29 rows in it from https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
+-- Use exec command to execute the stored procedure which should generate the table.
+
+
+
+-- QUERY 2
+-- Write a SQL Procedure to impement appropriate Foreign Key constraints on all 8 tables
+-- Orders, Employees, Shippers, Products, Customers, OrderDetails, Categories & Suppliers
+
+
+
+-- QUERY 3
+-- Write a query to add a new Product to Product Table with following info
+-- Product Name = "Vase", CategoryId = 9, Units = "12 boxes", Price = 500
+-- NOTE: Assuming there were Foreign Key constraints applied in Query 2, you ll have to 
+-- create a new Category with following info first 
+-- (CategoryID = 9, CategoryName = "Decoratives", Description = "Vase, PhotoFrames, Flowers"
+
+
+-- Query 4
+-- Write a command to update the Schema of Categories table and add 4th column 
+-- with column name 'Owner', where the values will be EmployeeID from Employees
+-- table.
+-- NOTE: Make sure to add the Foreign Key Constraint appropriately.
+
+
+
+-- Query 5
+-- Create and attach a trigger to Price changes in Products table which 
+-- should generate a warning if price is increased by  more than 50%
+
+
+-- Query 6
+-- Write a command to Increment the price of Product named 'Filo Mix' by 90%
+
+
+
+
+
